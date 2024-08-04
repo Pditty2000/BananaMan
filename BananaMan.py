017ece8a1ea968a4f7d21a711b527c98dd3fd311
Peter Johnston <pditty_2000@yahoo.com>
2024-08-03 17:34:52 -0700

    improving

--- BananaMan.py
+++ BananaMan.py
@@ -21,45 +21,37 @@ import pygame
 import sys
 from random import randint
 
-print("BANANAMAN!!!!!!!!!!!!")
-
-
 pygame.init()
 screen = pygame.display.set_mode((1080, 800))
-COLOR_INACTIVE = pygame.Color((200,200,200))
-COLOR_ACTIVE = pygame.Color((255,255,255))
-COLOR_UNFOUND = (0,0,250)
+screen_width = screen.get_width()
+screen_height = screen.get_height()
+BACKGROUND_COLOR = (0,0,200)
+COLOR_UNFOUND = (0,0,0)
 COLOR_FOUND = (0,255,255)
 FONT = pygame.font.Font(None, 64)
+smallFONT = pygame.font.Font(None, 32)
+FOUND_COUNT = 0
 
 
 
 class InputBox:
     def __init__(self, x, y, w, h, text=''):
         self.rect = pygame.Rect(x, y, w, h)
-        self.color = COLOR_INACTIVE
+        self.color = (200,200,200)
         self.text_color = (0,0,0)
         self.text = text
-        self.txt_surface = FONT.render(text, True, self.text_color)
-        self.active = False
+        self.txt_surface = FONT.render(self.text, True, self.text_color)
         self.submitted_text = ''
     def handle_event(self, event):
         if event.key == pygame.K_RETURN:
-            print(f'---> self.text: {self.text}')
             self.submitted_text = self.text
-            # check_letter(self.text)
             self.text = ''
         elif event.key == pygame.K_BACKSPACE:
             self.text = self.text[:-1]
         else:
             e_key = event.key
-            print(f'>>>>>>>>> e_key: {e_key}')
             if e_key in range(pygame.K_a, pygame.K_z + 1):
                 self.text = event.unicode.upper()
-            else:
-                print(f'NOT IN ALPHA')
-            # self.text = event.unicode.upper()
-            print(f'self.text: {self.text}, key pressed: {event.unicode}, key value: {e_key}')
         # Re-render the text.
         self.txt_surface = FONT.render(self.text, True, self.text_color)
     def update(self):
@@ -69,9 +61,7 @@ class InputBox:
         self.rect.w = width
         self.rect.h = height
     def draw(self, screen):
-        # Blit the rect.
-        pygame.draw.rect(screen, self.color, self.rect)
-        # Blit the text.
+        # pygame.draw.rect(screen, self.color, self.rect)
         screen.blit(self.txt_surface, (self.rect.x, self.rect.y))
 
 class LetterSpace:
@@ -79,49 +69,66 @@ class LetterSpace:
         self.rect = pygame.Rect(x, y, w, h)
         self.isfound = False
         self.text = text
-        self.color = COLOR_UNFOUND
-        self.text_color = (0,0,0)
+        self.color = (0,0,0)
+        self.text_color = COLOR_UNFOUND
         self.txt_surface = FONT.render(text, True, self.text_color)
     def found(self):
         self.isfound = True
         print(f'in found self.isfound: {self.isfound}')
         self.update()
     def update(self):
-        print(f'updating.... {self.isfound}')
         if self.isfound:
             self.color = COLOR_FOUND
-        
+    def draw(self, screen):
+        if self.text == ' ':
+            self.color = BACKGROUND_COLOR
+        pygame.draw.rect(screen, self.color, self.rect)
+        centerOffset = (self.rect.w - self.txt_surface.get_width())/2        
+        screen.blit(self.txt_surface, (self.rect.x + centerOffset, self.rect.y))
+
+class GuessCount:
+    def __init__(self, x, y, w, h):
+        self.count = 0
+        self.wrong = 0
+        self.right = 0
+        self.color = (150,150,150)
+        self.rect = pygame.Rect(x, y, w, h)
+        self.text_color = (0,0,0)
+        self.count_text = smallFONT.render(f'Total Guesses: {self.count}', True, self.text_color)
+        self.wrong_text = smallFONT.render(f'Incorrect: {self.wrong}', True, self.text_color)
+        self.right_text = smallFONT.render(f'Correct: {self.right}', True, self.text_color)
+    def guess(self):
+        self.count += 1
+        print(f'guess: self.count: {self.count}')
+
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, self.rect)
-        screen.blit(self.txt_surface, (self.rect.x, self.rect.y))
-
-def check_letter(letter, WORD):
-    find_index = WORD.find(letter)
-    if find_index == -1:
-        print(f'not found')
-    else:
-        print(f'letter found: {letter}, {find_index}, WORD.index: {WORD[find_index]}')
+        screen.blit(self.count_text, (self.rect.x+10, (screen_height-self.rect.h+5)))
+        screen.blit(self.wrong_text, (self.rect.x+10, self.rect.y+(self.rect.h/3)))
+        screen.blit(self.right_text, (self.rect.x+10, self.rect.y+(2*(self.rect.h/3))))
 
 
 def main():
     clock = pygame.time.Clock()
     input_box1 = InputBox((screen.get_width()/2), (screen.get_height()/2), 1, 1)
 
-    WORD = "This is a TEST"
-    WORD = WORD.upper()
+    # make phrase
+    PHRASE = "This is a TEST"
+    PHRASE = PHRASE.upper()
 
     # make letter spaces
     letter_spaces = []
-    word_length = len(WORD)
-
-    for i in range(word_length):
-        print(f'test: {i}, {WORD[i]} ++++++++> input box: {input_box1.text}')
-        letter_space = LetterSpace(((i*50)+100), 100, 40, 40, WORD[i])
+    phrase_length = len(PHRASE)
+    for i in range(phrase_length):
+        letter_space = LetterSpace(((i*50)+100), 100, 40, 40, PHRASE[i])
         letter_spaces.append(letter_space)
 
-    # input_boxes = [input_box1]
-    done = False
+    # make count area
+    guess_count = GuessCount((screen_width-(screen_width/4)), (screen_height-160), (screen_width/4), 160)
 
+
+    # start playing
+    done = False
     while not done:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -129,25 +136,27 @@ def main():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 done = True
             if event.type == pygame.KEYDOWN:
-                print(f'>>> input_box1.submitted_text: |{input_box1.submitted_text}|')
-                input_box1.handle_event(event)
+                e_key = event.key
+                if e_key in range(pygame.K_a, pygame.K_z + 1):
+                    print('it is a letter')
+                    input1 = event.unicode.upper()
+                    print(f'input1 = {input1}')
+                print(f'event: {event.unicode.upper()}')
+                input_box1.draw(screen)
+
                 if input_box1.submitted_text != '':
-                    print(f'<<<< input box has value: |{input_box1.submitted_text}|')
-                    for i in range(word_length):
+                    guess_count.guess()
+                    for i in range(phrase_length):
                         if letter_spaces[i].text == input_box1.submitted_text:
                             letter_spaces[i].found()
-                            print(f'{i}: letter_spaces[i]: {letter_spaces[i].text}, color: {letter_spaces[i].color}, found: {letter_spaces[i].isfound}')
-                            print('GARUNGA!!!')
-                # print(f'input_box1.submitted_text: {input_box1.submitted_text}')
 
             # if event.type == pygame.VIDEORESIZE:
             #     # There's some code to add back window content here.
             #     surface = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
-        screen.fill((0, 0, 250))
+        screen.fill(BACKGROUND_COLOR)
+        guess_count.draw(screen)
 
         input_box1.update()
-        # for letter in letter_spaces:
-        #     letter.update()
         input_box1.draw(screen)
         for letter in letter_spaces:
             letter.draw(screen)
