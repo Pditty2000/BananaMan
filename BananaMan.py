c38acbcb1c979389ff80813be927974e9df4d8be
Peter Johnston <pditty_2000@yahoo.com>
2024-08-06 22:08:12 -0700

    merge worthy

--- BananaMan.py
+++ BananaMan.py
@@ -33,36 +33,25 @@ smallFONT = pygame.font.Font(None, 32)
 FOUND_COUNT = 0
 
 
-
-class InputBox:
-    def __init__(self, x, y, w, h, text=''):
-        self.rect = pygame.Rect(x, y, w, h)
-        self.color = (200,200,200)
-        self.text_color = (0,0,0)
-        self.text = text
-        self.txt_surface = FONT.render(self.text, True, self.text_color)
-        self.submitted_text = ''
-    def handle_event(self, event):
-        if event.key == pygame.K_RETURN:
-            self.submitted_text = self.text
-            self.text = ''
-        elif event.key == pygame.K_BACKSPACE:
-            self.text = self.text[:-1]
-        else:
-            e_key = event.key
-            if e_key in range(pygame.K_a, pygame.K_z + 1):
-                self.text = event.unicode.upper()
-        # Re-render the text.
-        self.txt_surface = FONT.render(self.text, True, self.text_color)
-    def update(self):
-        # Resize the box if the text is too long.
-        width = max(10, self.txt_surface.get_width())
-        height = max(10, self.txt_surface.get_height())
-        self.rect.w = width
-        self.rect.h = height
-    def draw(self, screen):
-        # pygame.draw.rect(screen, self.color, self.rect)
-        screen.blit(self.txt_surface, (self.rect.x, self.rect.y))
+# class InputBox:
+#     def __init__(self, x, y, w, h, text=''):
+#         self.rect = pygame.Rect(x, y, w, h)
+#         self.color = (200,200,200)
+#         self.text_color = (0,0,0)
+#         self.text = text
+#         self.txt_surface = FONT.render(self.text, True, self.text_color)
+#         self.submitted_text = ''
+#         # Re-render the text.
+#         self.txt_surface = FONT.render(self.text, True, self.text_color)
+#     def update(self):
+#         # Resize the box if the text is too long.
+#         width = max(10, self.txt_surface.get_width())
+#         height = max(10, self.txt_surface.get_height())
+#         self.rect.w = width
+#         self.rect.h = height
+#     def draw(self, screen):
+#         # pygame.draw.rect(screen, self.color, self.rect)
+#         screen.blit(self.txt_surface, (self.rect.x, self.rect.y))
 
 class LetterSpace:
     def __init__(self, x, y, w, h, text=''):
@@ -74,7 +63,6 @@ class LetterSpace:
         self.txt_surface = FONT.render(text, True, self.text_color)
     def found(self):
         self.isfound = True
-        print(f'in found self.isfound: {self.isfound}')
         self.update()
     def update(self):
         if self.isfound:
@@ -86,31 +74,27 @@ class LetterSpace:
         centerOffset = (self.rect.w - self.txt_surface.get_width())/2        
         screen.blit(self.txt_surface, (self.rect.x + centerOffset, self.rect.y))
 
-class GuessCount:
+class StatBox:
     def __init__(self, x, y, w, h):
         self.count = 0
-        self.wrong = 0
-        self.right = 0
+        self.remaining = 0
+        self.correct = 0
         self.color = (150,150,150)
         self.rect = pygame.Rect(x, y, w, h)
         self.text_color = (0,0,0)
+    def update(self):
         self.count_text = smallFONT.render(f'Total Guesses: {self.count}', True, self.text_color)
-        self.wrong_text = smallFONT.render(f'Incorrect: {self.wrong}', True, self.text_color)
-        self.right_text = smallFONT.render(f'Correct: {self.right}', True, self.text_color)
-    def guess(self):
-        self.count += 1
-        print(f'guess: self.count: {self.count}')
-
+        self.correct_text = smallFONT.render(f'{self.correct} correct', True, self.text_color)
+        self.remain_text = smallFONT.render(f'{self.remaining} remaining', True, self.text_color)
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, self.rect)
         screen.blit(self.count_text, (self.rect.x+10, (screen_height-self.rect.h+5)))
-        screen.blit(self.wrong_text, (self.rect.x+10, self.rect.y+(self.rect.h/3)))
-        screen.blit(self.right_text, (self.rect.x+10, self.rect.y+(2*(self.rect.h/3))))
+        screen.blit(self.correct_text, (self.rect.x+10, self.rect.y+(self.rect.h/3)))
+        screen.blit(self.remain_text, (self.rect.x+10, self.rect.y+(2*(self.rect.h/3))))
 
 
 def main():
     clock = pygame.time.Clock()
-    input_box1 = InputBox((screen.get_width()/2), (screen.get_height()/2), 1, 1)
 
     # make phrase
     PHRASE = "This is a TEST"
@@ -124,10 +108,12 @@ def main():
         letter_spaces.append(letter_space)
 
     # make count area
-    guess_count = GuessCount((screen_width-(screen_width/4)), (screen_height-160), (screen_width/4), 160)
+    guess_count = StatBox((screen_width-(screen_width/4)), (screen_height-160), (screen_width/4), 160)
 
 
     # start playing
+    input1 = ''
+    submitted_input = ''
     done = False
     while not done:
         for event in pygame.event.get():
@@ -138,32 +124,53 @@ def main():
             if event.type == pygame.KEYDOWN:
                 e_key = event.key
                 if e_key in range(pygame.K_a, pygame.K_z + 1):
-                    print('it is a letter')
                     input1 = event.unicode.upper()
-                    print(f'input1 = {input1}')
-                print(f'event: {event.unicode.upper()}')
-                input_box1.draw(screen)
-
-                if input_box1.submitted_text != '':
-                    guess_count.guess()
+                if e_key == pygame.K_RETURN and input1 != '':
+                    guess_count.count += 1
+                    submitted_input = input1
+                    count = remaining = 0
                     for i in range(phrase_length):
-                        if letter_spaces[i].text == input_box1.submitted_text:
+                        if letter_spaces[i].text == submitted_input:
                             letter_spaces[i].found()
+                        if letter_spaces[i].isfound:
+                            count+=1
+                        else:
+                            if letter_spaces[i].text != ' ':
+                                remaining+=1
+                    print(f'count: {count}, remaining: {remaining}')
+                    guess_count.correct = count
+                    guess_count.remaining = remaining
+                    input1 = ''
 
+            input_letter = FONT.render(input1, True, (0,0,0))
+            submitted_letter = FONT.render(submitted_input, True, ((255,0,0)))
+            
+            # # if I need to make it resizeable
             # if event.type == pygame.VIDEORESIZE:
             #     # There's some code to add back window content here.
             #     surface = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
+
+        # get correct/missed stats
+        print(f'guess_count.remaining: {guess_count.remaining}')
+
+    # start the displaying   
+        # fill background color
         screen.fill(BACKGROUND_COLOR)
+
+        # show input letter (last key typed) and submitted letter
+        screen.blit(input_letter, (screen_width/2, screen_height/2))
+        screen.blit(submitted_letter, (screen_width/2, screen_height/2-40))
+
+        # show guess count totals
+        guess_count.update()
         guess_count.draw(screen)
 
-        input_box1.update()
-        input_box1.draw(screen)
+        # draw the answer letter boxes
         for letter in letter_spaces:
             letter.draw(screen)
 
         pygame.display.flip()
         clock.tick(30)
-
 
 if __name__ == '__main__':
     main()
