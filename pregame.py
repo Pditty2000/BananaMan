44ddd13619d8dba102ed8045693d9c314556beab
Peter Johnston <pditty_2000@yahoo.com>
2024-08-13 14:09:54 -0700

    moved word index out of running

--- pregame.py
+++ pregame.py
@@ -2,7 +2,7 @@ import pygame
 import random
 
 pygame.init()
-BACKGROUND_IMAGE = 'Images/Banana_yellow_background_474x266.png'
+# BACKGROUND_IMAGE = 'Images/Banana_yellow_background_474x266.png'
 ERROR_IMAGE = 'Images/error.png'
 BACKGROUND_COLOR = (0,255,0)
 TEXT_COLOR = (0,0,0)
@@ -11,8 +11,9 @@ TITLE_FONT = pygame.font.Font(None, 64)
 INSTRUCTION_TEXT = 'Press SPACE to begin...'
 INSTRUCTION_FONT = pygame.font.Font(None, 32)
 
-splash_image = pygame.image.load(BACKGROUND_IMAGE)
-splash_image = pygame.transform.scale(splash_image, (1080, 606))
+# splash_image = pygame.image.load(BACKGROUND_IMAGE)
+# splash_image = pygame.transform.scale(splash_image, (1080, 606))
+
 error_image = pygame.image.load(ERROR_IMAGE)
 error_image = pygame.transform.scale(error_image, (606, 606))
 splash_title = TITLE_FONT.render(TITLE_TEXT, True, TEXT_COLOR)
@@ -20,26 +21,17 @@ instructions = INSTRUCTION_FONT.render(INSTRUCTION_TEXT, True, TEXT_COLOR)
 title_rect = splash_title.get_rect()
 inst_rect = instructions.get_rect()
 
-def splashscreen(screen, word_list):
-    test_count = 0
-    # for event in pygame.event.get():
-        # if event.type == pygame.MOUSEBUTTONDOWN:
-        #     test_count +=1
-        #     print(f'THIS IS A MOUSECLICK: {test_count}')
+def show_word_list(screen, word_list):
+    # print(f'show_word_list: {word_list}')
     screen_rect = screen.get_rect()
-    print(f'landing page word list: {word_list}')
-    selected_word = "TEST"
     list_length = len(word_list)
-    screen.blit(splash_image, (0, 0))
-    screen.blit(splash_title, (screen_rect.centerx - (title_rect.width/2), screen_rect.centery))
-    screen.blit(instructions, ((screen_rect.centerx-(inst_rect.width/2)), (screen_rect.height-(inst_rect.height))))
-    # show words list on splash screen
     for i in range(list_length):
         word = word_list[i]
         word_image = word_bubble(word)
         screen.blit(word_image, (0, (i*40)))
-    pygame.display.flip()
-    return selected_word
+    screen.blit(instructions, 
+                ((screen_rect.centerx-(inst_rect.width/2)), 
+                 (screen_rect.height-(inst_rect.height))))
 
 def errorscreen(screen):
     error_rect = error_image.get_rect()
@@ -67,3 +59,10 @@ def word_bubble(word):
     word_text = f'{word_length} {word}'
     word_image = pygame.font.Font(None, 32).render(word_text, True, TEXT_COLOR)
     return  (word_image)
+
+def loadScreen(screen, bg_image):
+    screen_rect = screen.get_rect()
+    splash_image = pygame.image.load(bg_image)
+    splash_image = pygame.transform.scale(splash_image, (1080, 606))
+    screen.blit(splash_image, (0,0))
+    screen.blit(splash_title, (screen_rect.centerx - (title_rect.width/2), screen_rect.centery))
